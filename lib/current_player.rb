62412d9e7c441442e4cda9a605df1b1f5d238339
Kiefer Ares <kiefer_aresii@dlsu.edu.ph>
2020-06-11 11:31:51 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,9 @@
 def turn_count(board)
   count = 0
-  board.each { |b| 
-    if b != " " 
-      count += 1 
-    end 
+  board.each { |b|
+    if b != " "
+      count += 1
+    end
   }
   count
 end
