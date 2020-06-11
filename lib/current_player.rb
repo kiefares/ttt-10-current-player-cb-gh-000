a6e8d6f2ca6b62e39c02eb4d4f0d7d874b14ff20
Kiefer Ares <kiefer_aresii@dlsu.edu.ph>
2020-06-11 11:24:33 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,12 @@
 def turn_count(board)
   count = 0
-  board.each { |b| turn_count = b = "X" || b = "O" ? turn_count + 1 : turn_count }
+  board.each { |b| count = b = "X" || b = "O" ? count + 1 : count }
 end
 
 def current_player(board)
-
+  turn = turn_count(board)
+  if turn % 2 = 0
+    "X"
+  else
+    "O"
 end
